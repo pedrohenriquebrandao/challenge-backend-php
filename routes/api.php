38ff112953aca3e7e1ac2840791d5c3579fdd6db
Pedro Brandão <phbrandao@outlook.com>
2023-09-18 18:54:58 -0300

    Configurando autenticação JWT

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function(Request $request) {
+    $credentials = $request->only(['email', 'password']);
+
+    if(!$token = auth()->attempt($credentials)) {
+        abort(401, 'Unauthorized');
+    } 
+ 
+    return response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]
+    ]);
 });
